64b5a667d897f08a7dfb42391395d14e39c4db03
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-08-10 22:58:00 +0530

    Working on header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Header() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <NavLink>Login</NavLink>
+    </nav>
+  );
 }
 
 export default Header;
