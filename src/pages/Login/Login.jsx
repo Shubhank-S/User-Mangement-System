1a19f6008c762d7b10e44acdc75ef45e9e3474a5
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-08-11 01:48:33 +0530

    Handled From data in login component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
   return (
     <>
-      <form className="form">
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" />
+      <form className="form" onSubmit={handleSubmit}>
+        <h1>USER MANGEMENT</h1>
+        <input
+          value={email}
+          className="input"
+          type="email"
+          placeholder="Enter Your Email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          value={password}
+          className="input"
+          type="password"
+          placeholder="Enter Your Password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="button">Submit</button>
       </form>
     </>
   );
